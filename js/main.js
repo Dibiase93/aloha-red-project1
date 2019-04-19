2ef0be2d7ab21b6513e23461686d3763d7557e76
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-04-19 12:12:47 -0700

    added javascript carosal

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() { 
+
+    var flkty = new Flickity( '.main-carousel', {
+        cellAlign: 'center',
+        // container: true,
+        freeScroll: true,
+        // contain: true,
+        fullscreen: true,
+
+      });
+    // const subscribeFrom.addEventListener()
+    //all of code inside
+
+    //  if ( check input value != ''){
+
+
+    // }
+
+});
